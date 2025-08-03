2a67525410d2a64a17e9f5e432835a0951fd4ab6
zhec <192538093+zhec9p@users.noreply.github.com>
2025-08-03 15:40:03 -0400

    Spider Silk: OHKO chance effect

--- 00100 Abilities/01003 SpiderSilk.rb
+++ 00100 Abilities/01003 SpiderSilk.rb
@@ -46,7 +46,13 @@ module Battle
           return if @attacked.empty?
 
           scene.visual.show_ability(@target)
-          @attacked.uniq.each { |battler| logic.stat_change_handler.stat_change_with_process(:eva, -2, battler, @target) }
+          @attacked.uniq!
+
+          @attacked.each do |battler|
+            logic.stat_change_handler.stat_change_with_process(:eva, -1, battler, @target)
+            battler.effects.add(Effects::SpiderSilkMark.new(logic, battler)) unless battler.effects.has?(:spider_silk_mark)
+          end
+
           @attacked.clear
         end
       end
@@ -63,9 +69,68 @@ module Battle
         return if !with.grounded? || with.hold_item?(:heavy_duty_boots)
 
         handler.scene.visual.show_ability(origin)
-        handler.logic.stat_change_handler.stat_change_with_process(
-          :eva, -2, with, with.has_ability?(:mirror_armor) ? origin : nil
-        )
+        launcher = with.has_ability?(:mirror_armor) ? origin : nil
+        handler.logic.stat_change_handler.stat_change_with_process(:eva, -1, with, launcher)
+        with.effects.add(Effects::SpiderSilkMark.new(handler.logic, with)) unless with.effects.has?(:spider_silk_mark)
+      end
+    end
+
+    class SpiderSilkMark < PokemonTiedEffectBase
+      # Create a new SpiderSilkMark effect
+      # @param logic [Battle::Logic]
+      # @param target [PFM::PokemonBattler]
+      def initialize(logic, target)
+        super
+        logic.scene.display_message_and_wait(message(target))
+      end
+
+      # Get the name of the effect
+      # @return [Symbol]
+      def name
+        return :spider_silk_mark
+      end
+
+      # Chance of the OHKO move hitting
+      # @param user [PFM::PokemonBattler] User of the move
+      # @param target [PFM::PokemonBattler] Target of the move
+      # @param move [Battle::Move]
+      # @return [Float]
+      def ohko_chance(user, target, move)
+        return 0 unless move.ohko?
+        return move.zhec_battle__chance_of_hit(user, target) if target != @pokemon
+
+        # 30% baseline for higher level is intentional
+        chance = [0, user.level - target.level].max + 30
+        chance *= move.evasion_mod(target)
+        return chance
+      end
+
+      private
+
+      # Message to display when Spider Silk's OHKO chance effect is applied to the target
+      # @param target [PFM::PokemonBattler]
+      # @return [String]
+      def message(target)
+        return parse_text_with_pokemon(1000, 18, target)
+      end
+    end
+  end
+
+  class Move
+    class OHKO
+      # Tell if the move is an OHKO move
+      # @return [Boolean]
+      def ohko?
+        return true
+      end
+
+      alias zhec_battle__chance_of_hit chance_of_hit
+      def chance_of_hit(user, target)
+        chance = zhec_battle__chance_of_hit(user, target)
+        effect = target.effects.get(:spider_silk_mark)
+        return chance unless effect && chance < 100
+
+        return effect.ohko_chance(user, target, self)
       end
     end
   end
