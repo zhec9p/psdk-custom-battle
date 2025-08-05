b38dc5279bca1d38d1e6968191ece59f67b482b4
zhec <192538093+zhec9p@users.noreply.github.com>
2025-08-04 22:55:24 -0400

    More comment cleanup

--- 00000 Adjustments/00010 Battle/00051 Moves/01000 ExpandingForce.rb
+++ 00000 Adjustments/00010 Battle/00051 Moves/01000 ExpandingForce.rb
@@ -2,8 +2,8 @@ module Battle
   class Move
     class ExpandingForce
       # Test if the effect is working
-      # @param user [PFM::PokemonBattler] User of the move
-      # @param actual_targets [Array<PFM::PokemonBattler>] Targets that will be affected by the move
+      # @param user [PFM::PokemonBattler]
+      # @param actual_targets [Array<PFM::PokemonBattler>]
       # @return [Boolean]
       def effect_working?(user, actual_targets)
         return user.grounded? && @logic.field_terrain_effect.psychic?
